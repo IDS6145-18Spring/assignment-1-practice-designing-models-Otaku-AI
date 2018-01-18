e3371b155fcef04dc1378bf315dc5240961b40fe
Otaku-AI <rfatt13@gmail.com>
2018-01-18 13:42:33 -0500

    Fixed Indentation error

--- code/streetcongestion/agent.py
+++ code/streetcongestion/agent.py
@@ -1,20 +1,21 @@
 class Agent:
-	
-	def __init_(self, destination,adaptability, priority):
-		self.destination = destination #where the user is headed
-		self.adaptability = adaptability #how willing they are to change their current plans
-		self.priority = priority #what priority they have in the queue
-	
-	#sends a message to the system
-	def sendMessage(string):
-		return
-	
-	#This function receives messages for the system
-	def receiveMessage():
-	#Initialize empty message
-	currentMessage = ''
-		return currentMessage
-	
-	#Returns results of utility function
-	def getUtility():
-		return 
+
+    def __init_(self, destination, adaptability, priority):
+        self.destination = destination  # where the user is headed
+        # how willing they are to change their current plans
+        self.adaptability = adaptability
+        self.priority = priority  # what priority they have in the queue
+
+    # sends a message to the system
+    def sendMessage(string):
+        return
+
+    # This function receives messages for the system
+    def receiveMessage():
+        # Initialize empty message
+        currentMessage = ''
+        return currentMessage
+
+    # Returns results of utility function
+    def getUtility():
+        return
