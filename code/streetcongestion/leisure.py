5c14ccedf198d4c00270fbe14ed07cdc6ed78016
Otaku-AI <rfatt13@gmail.com>
2018-01-18 13:32:23 -0500

    Added comments and fixed function

--- code/streetcongestion/leisure.py
+++ code/streetcongestion/leisure.py
@@ -2,17 +2,20 @@ from agent import Agent
 class Leisure(Agent):
 
 	def __init_(self, availability, offerings, contact, events, reviews):
-		self.availability = availability
-		self.offerings = offerings
-		self.contact = contact
-		self.events = events
-		self.reviews = reviews
+		self.availability = availability #when the agents available to do system functions
+		self.offerings = offerings #what the agent offers
+		self.contact = contact #how to contact the agent
+		self.events = events #what events the agent already has planne
+		self.reviews = reviews #reviews for the agent
 
+	#allows the agent to send events to advertise on the system
 	def advertise(ad):
 		return
 
-	def messageContact(message, contact):
+	#allows a message to be sent to the agent
+	def messageContact(message):
 		return
 
-	def acceptEvent():
-		return True
+	#allows an offered event to be accepted or denied
+	def acceptEvent(acceptbool):
+		return
