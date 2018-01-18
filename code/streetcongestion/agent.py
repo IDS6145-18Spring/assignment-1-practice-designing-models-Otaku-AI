13065e8c25d3a02f38bd89c203626aa26b35815b
Otaku-AI <rfatt13@gmail.com>
2018-01-18 13:24:12 -0500

    Added comments and fixed some functions

--- code/streetcongestion/agent.py
+++ code/streetcongestion/agent.py
@@ -1,13 +1,20 @@
 class Agent:
-	currentMessage = ''
-
+	
 	def __init_(self, destination,adaptability, priority):
-		self.destination = destination
-		self.adaptability = adaptability
-		self.priority = priority
-	def sendMessage():
-		return currentMessage
-	def receiveMessage(message):
+		self.destination = destination #where the user is headed
+		self.adaptability = adaptability #how willing they are to change their current plans
+		self.priority = priority #what priority they have in the queue
+	
+	#sends a message to the system
+	def sendMessage(string):
 		return
+	
+	#This function receives messages for the system
+	def receiveMessage():
+	#Initialize empty message
+	currentMessage = ''
+		return currentMessage
+	
+	#Returns results of utility function
 	def getUtility():
 		return 
